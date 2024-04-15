49b27f19f2f997c839a567fdcf073b6a2093db88
Gopi Srinivasan <igopisrinivasan@gmail.com>
2024-04-15 10:30:38 +0530

    Solution

--- cypress/integration/DateRangePicker.spec.ts
+++ cypress/integration/DateRangePicker.spec.ts
@@ -1,6 +1,38 @@
+const currentDate = new Date();
+
 describe('DateRangePicker', () => {
-  it('should render', () => {
-    cy.visit('/');
-    cy.get('.DateRangeSelect').should('be.visible');
-  });
+
+  const formattedDate = currentDate.toLocaleString('en-US', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit'
 });
+    beforeEach(() => {
+      cy.visit('/');
+      cy.viewport(1920, 1080);
+    })
+
+  it('should render', () => {
+     cy.get('.DateRangeSelect').should('be.visible');
+  });
+
+  ['#mui-4','#mui-5'].forEach(element => {
+    it('should show calendar when clicking on Date Fields   :' + element, () => {
+      cy.get(element).click({force: true});
+      cy.get('[role="tooltip"]').should('be.visible',"Verify calendar is visible");
+    });
+  });
+
+  it('verify able to select and Enter date range in both start and end field', () => {
+    cy.get('#mui-4').click({force: true});
+    cy.get('[role="tooltip"]').should('be.visible',"Verify calendar is visible");
+    cy.get('button.MuiDateRangePickerDay-day').contains(currentDate.toLocaleString('en-US', {day: '2-digit'})).click();
+    cy.get('#mui-4').should('have.value', formattedDate).click();
+    cy.get('#mui-5').click({force: true});
+    cy.get('#mui-5').type(formattedDate).should('have.value', formattedDate).click();
+    cy.get('#mui-4').should('have.value', formattedDate)
+  })
+
+  
+});
+
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -19,4 +20,6 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  allureWriter(on, config);
+    return config;
 }
